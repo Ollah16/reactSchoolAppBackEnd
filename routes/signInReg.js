e1c8573014fae4d96b958a443f10add5017f093a
Ollah16 <olaoluwaboluwatife@yahoo.com>
2023-08-31 16:21:47 -0400

    cor4

--- routes/signInReg.js
+++ routes/signInReg.js
@@ -1,6 +1,10 @@
 const express = require('express')
-const { handleRegistration, handleLogin } = require('../control/loginSignUpControl')
+// const { handleRegistration, handleLogin } = require('../control/loginSignUpControl')
 const router = express.Router()
-router.post('/register', handleRegistration)
-router.post('/login', handleLogin)
+// router.post('/register', handleRegistration)
+// router.post('/login', handleLogin)
+router.post('/login', (req, res) => {
+    res.send('success')
+})
+
 module.exports = router
