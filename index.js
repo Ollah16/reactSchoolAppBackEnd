757eaee4cd7a15ce35da8617c345940d9aeb13bc
Ollah16 <olaoluwaboluwatife@yahoo.com>
2023-08-31 16:16:17 -0400

    cors

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const cors = require("cors")
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 const signInReg = require("./routes/signInReg")
-// const tutor = require("./routes/tutorRoute")
-// const student = require('./routes/studentRoute')
-// app.use('/signInReg', signInReg)
-// app.use('/tutor', tutor)
-// app.use('/student', student)
-app.get('/', (req, res) => {
-    res.send('success')
-})
+const tutor = require("./routes/tutorRoute")
+const student = require('./routes/studentRoute')
+app.use('/signInReg', signInReg)
+app.use('/tutor', tutor)
+app.use('/student', student)
+// app.get('/', (req, res) => {
+//     res.send('success')
+// })
 const port = process.env.PORT || 9090
 app.listen(port, () => {
 })
--- routes/signInReg.js
+++ routes/signInReg.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { handleRegistration, handleLogin, handleAddToModules } = require('../control/loginSignUpControl')
+const { handleRegistration, handleLogin } = require('../control/loginSignUpControl')
 const router = express.Router()
 router.post('/register', handleRegistration)
 router.post('/login', handleLogin)
