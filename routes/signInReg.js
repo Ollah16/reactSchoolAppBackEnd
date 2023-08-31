99df2b2ef6bdb8c6fc800790752ef9b3f9b1d066
Ollah16 <olaoluwaboluwatife@yahoo.com>
2023-08-31 16:52:29 -0400

    logt

--- routes/signInReg.js
+++ routes/signInReg.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { handleLoginFunction } = require('../control/loginSignUpControl')
+// const { handleLoginFunction } = require('../control/loginSignUpControl')
 const router = express.Router()
 // router.post('/register', handleRegistration)
 // router.post('/login', handleLoginFunction)
