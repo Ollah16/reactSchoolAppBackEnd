773fa56314cc10448cf3e0e0c4e4810340cc9910
Ollah16 <olaoluwaboluwatife@yahoo.com>
2023-09-01 12:20:25 -0400

    ser

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ if (process.env.Node_ENV != "production") require("dotenv").config()
 const express = require("express")
 const app = express()
 const cors = require("cors")
-app.use(cors())
+app.use(cors({ origin: 'https://school-app-e4abd.web.app/' }))
 app.use(express.urlencoded({ extended: true }))
 const regLog = require("./routes/signInUpRoute")
 const tutor = require("./routes/tutorRoute")
@@ -10,9 +10,6 @@ const student = require("./routes/studentRoute")
 app.use('/signinup', regLog)
 app.use('/tutor', tutor)
 app.use('/student', student)
-app.use('/', (req, res) => {
-    res.send('hi')
-})
 const port = process.env.PORT || 9090
 app.listen(port, () => {
 })
