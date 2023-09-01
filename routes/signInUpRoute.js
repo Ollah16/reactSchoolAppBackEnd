33e2795193f122d80c34bab147cfb8d8f0e5aa5c
Ollah16 <olaoluwaboluwatife@yahoo.com>
2023-09-01 13:36:36 -0400

    verwer

--- routes/signInUpRoute.js
+++ routes/signInUpRoute.js
@@ -1,9 +1,9 @@
 const express = require("express")
 const { handleSignIn, handleSignUp } = require("../control/signInAndUp")
 const router = express.Router()
-// router.post('/signIn', handleSignIn)
-router.post('/signIn', (req, res) => {
-    res.send('hi')
-})
+router.post('/signIn', handleSignIn)
+// router.post('/signIn', (req, res) => {
+//     res.send('hi')
+// })
 router.post('/register', handleSignUp)
 module.exports = router
