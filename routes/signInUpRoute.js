2580270339364475f089202c516323aa7a425c3d
Ollah16 <olaoluwaboluwatife@yahoo.com>
2023-09-01 10:34:24 -0400

    move

--- routes/signInUpRoute.js
+++ routes/signInUpRoute.js
@@ -1,8 +1,6 @@
 const express = require("express")
-const { handleHi } = require("./logInUp")
-// const { handleSignIn, handleSignUp } = require("../control/singInUpControl")
+const { handleSignIn, handleSignUp } = require("../control/singInUpControl")
 const router = express.Router()
-// router.post('/signIn', handleSignIn)
-// router.post('/register', handleSignUp)
-router.post('/', handleHi)
+router.post('/signIn', handleSignIn)
+router.post('/register', handleSignUp)
 module.exports = router
