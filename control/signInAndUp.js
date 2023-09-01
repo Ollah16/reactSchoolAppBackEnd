9a43818beaf3758e74d44b2ebbda4db558c40bfd
Ollah16 <olaoluwaboluwatife@yahoo.com>
2023-09-01 10:46:38 -0400

    bcrypt

--- control/signInAndUp.js
+++ control/signInAndUp.js
@@ -1,5 +1,5 @@
 const { Students, Tutors, AllModules } = require('../model/schoolData');
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const jwtSecretKey = process.env.JWTSECRETKEY;
 
