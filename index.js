d10fd481df24cc5761603ddcd271e9eceaac7ca7
Ollah16 <olaoluwaboluwatife@yahoo.com>
2023-09-01 05:14:57 -0400

    u1

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 const tutor = require("./routes/tutorRoute")
 const student = require('./routes/studentRoute')
-const signupin = require("./routes/signInUpRoute")
+// const signupin = require("./routes/signInUpRoute")
 app.use('/tutor', tutor)
 app.use('/student', student)
-app.use('/signInUp', signupin)
+// app.use('/signInUp', signupin)
 
 const port = process.env.PORT || 9090
 app.listen(port, () => {
